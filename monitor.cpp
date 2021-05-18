724504a1cd08b641cf6da71935be27c5b7c984dc
loukaspe20 <loukaspe20@gmail.com>
2021-05-18 08:29:26 +0300

    Monitor takes pipe buffer size from pipe

--- monitor.cpp
+++ monitor.cpp
@@ -3,22 +3,28 @@
 
 using namespace std;
 
-char **getPipeNamesFromCommandLineArguments(int, char **);
-
 int main(int argc, char **argv) {
-    char **pipeNames = getPipeNamesFromCommandLineArguments(argc, argv);
-}
+    char* pipeNameForReading = argv[1];
+    char* pipeNameForWriting = argv[2];
+    int fd;
 
-char **getPipeNamesFromCommandLineArguments(int argc, char **argv) {
-    // number of pipes passed from TravelMonitor equals to the argc - 1 (the
-    // executable file name)
-    int numberOfPipeNames = argc - 1;
-    char **pipeNames = (char **) malloc(numberOfPipeNames * sizeof(char *));
+    cout << "--" << pipeNameForReading << endl;
+    cout << "--" << pipeNameForWriting << endl;
 
-    for (int i = 1; i < argc; i++) {
-        pipeNames[i - 1] = argv[i];
-        printf("%s\n", pipeNames[i-1]);
-    }
+    PipeWriter* pipeWriter = new PipeWriter(
+        fd,
+        pipeNameForWriting
+    );
 
-    return pipeNames;
+    PipeReader* pipeReader = new PipeReader(
+        fd,
+        pipeNameForReading
+    );
+
+    int bufferSize = pipeReader->readNumberWithBlock();
+    pipeReader->setBufferSize(bufferSize);
+    pipeWriter->setBufferSize(bufferSize);
+    cout << "oo " << bufferSize << endl;
+
+    return 0;
 }
